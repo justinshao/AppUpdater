e054cddd747af1440180036ad0e1af34909da855
shao <justinshao1258@outlook.com>
2018-09-05 11:16:25 +0800

    使用系统的字符串比较器

--- shared/DicIgnoreCase.cs
+++ shared/DicIgnoreCase.cs
@@ -6,30 +6,7 @@ namespace Justin.Updater.Shared
     class DicIgnoreCase<T> : Dictionary<string, T>
     {
         public DicIgnoreCase() :
-            base(StringComparer.Instance)
+            base(StringComparer.OrdinalIgnoreCase)
         { }
-    }
-    class StringComparer : IEqualityComparer<string>
-    {
-        internal static readonly StringComparer Instance = new StringComparer();
-
-        public bool Equals(string x, string y)
-        {
-            if (ReferenceEquals(x, y))
-                return true;
-
-            if (x == null || y == null)
-                return false;
-
-            if (x.Length != y.Length)
-                return false;
-
-            return x.Equals(y, StringComparison.OrdinalIgnoreCase);
-        }
-
-        public int GetHashCode(string obj)
-        {
-            return obj.ToUpper().GetHashCode();
-        }
     }
 }
